570e3453c82ec7c5a872cb2dbd865161f413c16f
Vinicius <vinicius-fonseca@users.noreply.github.com>
2016-05-21 10:52:20 -0300

    Implemented linear time sorting algorithms.
    Also, two sorting algorithms by comparison (InsertionSort and QuickSort)
    were implemented.

---
+++ src/edu/ufmg/dcc849/comparedSort/InsertionSort.java
@@ -0,0 +1,7 @@
+package edu.ufmg.dcc849.comparedSort;
+
+/**
+ * Created by Vinicius on 21/05/2016.
+ */
+public class InsertionSort {
+}
---
+++ src/edu/ufmg/dcc849/linearSort/RadixSort.java
@@ -0,0 +1,7 @@
+package edu.ufmg.dcc849.linearSort;
+
+/**
+ * Created by Vinicius on 21/05/2016.
+ */
+public class RadixSort {
+}
