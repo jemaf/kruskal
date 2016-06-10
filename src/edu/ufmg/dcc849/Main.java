6c5b31b45b786da6ff4b35eadfe444f0d74236e4
Vinicius <vinicius-fonseca@users.noreply.github.com>
2016-06-10 09:52:37 -0300

    Implemented Main class for running experiments.
    Also, it was implemented a class for Confidence Interval Calculation,
    which considers Student's t-Distribution.

--- src/edu/ufmg/dcc849/Main.java
+++ src/edu/ufmg/dcc849/Main.java
@@ -5,10 +5,9 @@ import edu.ufmg.dcc849.sort.Sort;
 import edu.ufmg.dcc849.sort.comparison.QuickSort;
 import edu.ufmg.dcc849.sort.linear.BucketSort;
 import edu.ufmg.dcc849.sort.linear.CountingSort;
+import edu.ufmg.dcc849.util.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by edumontandon on 5/10/16.
@@ -16,27 +15,63 @@ import java.util.List;
 public class Main {
 
     public static void main(String args[]) throws IOException {
+        Integer maxVertexNum = Integer.parseInt(args[0]);
+        Integer caseNum = Integer.parseInt(args[1]);
+        Integer maxRunTime = Integer.parseInt(args[2]);
+        Double tValue = Double.parseDouble(args[3]);
 
-        CompleteGraphGenerator generator = new CompleteGraphGenerator(500, 50000, 1);
-        Graph testGraph = generator.run();
-
-        List<Edge<Integer>> edgeList = testGraph.getEdges();
-
-        System.out.println("Original Edges:");
-
-        for (Edge<Integer> edge : edgeList) {
-            System.out.println(edge.getOrigin().getName() + " -> " + edge.getDestiny().getName() + " = " + edge.getValue());
-        }
+        //Execution time for each run and each algorithm
+        Double[] execTimeCounting = new Double[maxRunTime];
+        Double[] execTimeQuick = new Double[maxRunTime];
+        Double[] execTimeBucket = new Double[maxRunTime];
 
         Sort<Edge<Integer>> countingSort = new CountingSort();
-        Kruskal<Integer> kruskal = new Kruskal<Integer>(countingSort);
+        Sort<Edge<Integer>> bucketSort = new BucketSort();
+        Sort<Edge<Integer>> quickSort = new QuickSort();
 
-        List<Edge<Integer>> mst = kruskal.execute(testGraph);
+        Kruskal<Integer> kruskalCounting = new Kruskal<Integer>(countingSort);
+        Kruskal<Integer> kruskalBucket = new Kruskal<Integer>(bucketSort);
+        Kruskal<Integer> kruskalQuick = new Kruskal<Integer>(quickSort);
 
-        System.out.println("Edges from the MST:");
+        Long startTime, endTime; //Start and end timestamp for a single run
 
-        for (Edge<Integer> edge : mst) {
-            System.out.println(edge.getOrigin().getName() + " -> " + edge.getDestiny().getName() + " = " + edge.getValue());
+        System.out.println("Vertex_Num\tAvg_Couting\tIC_Counting\tAvg_Bucket\tIC_Bucket\tAvg_Quick\tIC_Quick");
+
+        for(Integer currVertexNum = 10; currVertexNum <= maxVertexNum; currVertexNum += 10){ //Varies the number of vertices
+            /*
+             * Execute the algorithms for different instances of graphs with the same settings
+             */
+            for(Integer currRunTime = 0; currRunTime < maxRunTime; currRunTime++) {
+                CompleteGraphGenerator currGen = new CompleteGraphGenerator(currVertexNum,
+                        2 * currVertexNum, caseNum);
+                Graph currGraphInstance = currGen.run();
+
+                startTime = System.currentTimeMillis();
+                kruskalCounting.execute(currGraphInstance);
+                endTime = System.currentTimeMillis();
+                execTimeCounting[currRunTime] = Double.valueOf(endTime - startTime);
+
+                startTime = System.currentTimeMillis();
+                kruskalBucket.execute(currGraphInstance);
+                endTime = System.currentTimeMillis();
+                execTimeBucket[currRunTime] = Double.valueOf(endTime - startTime);
+
+                startTime = System.currentTimeMillis();
+                kruskalQuick.execute(currGraphInstance);
+                endTime = System.currentTimeMillis();
+                execTimeQuick[currRunTime] = Double.valueOf(endTime - startTime);
+            }
+
+            Statistics countingStats = new Statistics(execTimeCounting);
+            Statistics bucketStats = new Statistics(execTimeBucket);
+            Statistics quickStats = new Statistics(execTimeQuick);
+
+            String result = currVertexNum + "\t" +
+                    countingStats.getMean() + "\t" + countingStats.getConfidenceInterval(tValue) + "\t" +
+                    bucketStats.getMean() + "\t" + bucketStats.getConfidenceInterval(tValue) + "\t" +
+                    quickStats.getMean() + "\t" + quickStats.getConfidenceInterval(tValue);
+
+            System.out.println(result);
         }
     }
 }
---
+++ src/edu/ufmg/dcc849/util/Statistics.java
@@ -0,0 +1,37 @@
+package edu.ufmg.dcc849.util;
+
+/**
+ * Created by Vinicius on 10/06/2016.
+ */
+public class Statistics {
+    private Double[] data;
+    private int size;
+
+    public Statistics(Double[] data){
+        this.data = data;
+        this.size = data.length;
+    }
+
+    public Double getMean(){
+        Double sum = 0.0;
+        for(Double a : this.data)
+            sum += a;
+        return sum/this.size;
+    }
+
+    public Double getVariance(){
+        Double mean = getMean();
+        Double temp = 0.0;
+        for(Double a : this.data)
+            temp += (mean-a)*(mean-a);
+        return temp/this.size;
+    }
+
+    public Double getStdDev(){
+        return Math.sqrt(getVariance());
+    }
+
+    public Double getConfidenceInterval(Double tValue){
+        return tValue*(getStdDev()/this.size);
+    }
+}
